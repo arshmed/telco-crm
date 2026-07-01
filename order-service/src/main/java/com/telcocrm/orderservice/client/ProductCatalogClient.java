8a853b267f1f70fd03738f6327a07b45b3b70f43
Orhan Gölcür <orhangolcur0@gmail.com>
2026-07-01 13:01:43 +0300

    feat(order-service): add resilience4j circuit breaker to feign clients

--- order-service/src/main/java/com/telcocrm/orderservice/client/ProductCatalogClient.java
+++ order-service/src/main/java/com/telcocrm/orderservice/client/ProductCatalogClient.java
@@ -1,13 +1,21 @@
 package com.telcocrm.orderservice.client;
 
 import com.telcocrm.orderservice.client.dto.ProductResponse;
+import com.telcocrm.orderservice.exception.ServiceUnavailableException;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @FeignClient(name = "product-catalog-service", path = "/api/v1")
 public interface ProductCatalogClient {
 
     @GetMapping("/tariffs/{code}")
+    @CircuitBreaker(name = "productCatalogServiceCircuitBreaker", fallbackMethod = "getProductByCodeFallback")
     ProductResponse getProductByCode(@PathVariable String code);
+
+    default ProductResponse getProductByCodeFallback(String code, Throwable throwable) {
+        throw new ServiceUnavailableException("Product catalog service", "PRODUCT_CATALOG_SERVICE_UNAVAILABLE", throwable);
+    }
 }
